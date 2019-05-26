62c5c82fdd59f20fb0f4bfffe568aaf87455edaf
Sachin <sachinb443@gmail.com>
2019-05-26 21:13:14 +0530

    Developed Create user component

--- src/components/create-user.component.js
+++ src/components/create-user.component.js
@@ -1,10 +1,41 @@
 import React, { Component } from 'react'
 
 export default class CreateUser extends Component{
+    constructor(props)
+    {
+        super(props);
+        this.onChangeUsername = this.onChangeUsername.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+        this.state = {
+            username:''
+        }
+    }
+
+    onChangeUsername(e){
+        this.setState({
+            username :  e.target.value
+        })
+        console.log(this.state.username);
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        const user = {
+            username:this.state.username
+        }
+        console.log(user);
+    }
+
     render(){
         return(
             <div className="container">
-                <p>You are on Create User Page</p>
+                <form onSubmit={this.onSubmit}>
+                    <div className="form-group">
+                        <label >UserName: </label>
+                        <input type="text" className="form-control" placeholder="Enter New Username" required onChange={this.onChangeUsername} />
+                    </div>
+                    <button type="submit" className="btn btn-primary" >Submit</button>
+                </form>
             </div>
         )
     }
